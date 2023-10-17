19273c9e8e6045b95e188c2f502b84ab601f536d
TonerowEEtim <anthony.eetim@gmail.com>
2023-10-17 15:54:48 +0200

    0x03-log_parsing Tasks

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,55 +1,44 @@
 #!/usr/bin/python3
 
+"""Script that reads stdin line by line and computes metrics"""
+
 import sys
 
 
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
+def printsts(dic, size):
+    """ WWPrints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
 
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+               "404": 0, "405": 0, "500": 0}
 
+count = 0
+size = 0
 
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0}
 try:
-    for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
+     for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
 
-        if len(parsed_line) > 2:
-            counter += 1
+        stlist = line.split()
+        count += 1
 
+            try:
+                size += int(stlist[-1])
+            except:
+                pass
+            try:
+                if stlist[-2] in sts:
+                    sts[stlist[-2]] += 1
+            except:
+                pass
+    printsts(sts, size)
 
-            if counter <= 10:
-            total_file_size += int(parsed_line[0])  # file size
-            code = parsed_line[1]  # status code
+except KeyboardInterrupt:
+    printsts(sts, size)
+    raise
 
-            if (code in dict_sc.keys()):
-                dict_sc[code] += 1
-
-        if (counter == 10):
-            print_msg(dict_sc, total_file_size)
-            counter = 0
-
-finally:
-        print_msg(dict_sc, total_file_size)
